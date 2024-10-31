48a8a04252a276b5f1cf44276683b35327af1f73
Mr-Pine <git@mr-pine.de>
2024-10-31 19:45:56 +0100

    Reformat flake and use builtin forAllSystems

--- flake.nix
+++ flake.nix
@@ -2,24 +2,60 @@
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-		  src = ./.;
-          nativeBuildInputs = with pkgs; [ openssl pkg-config  dbus ];
-		};
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-		  nativeBuildInputs = [ openssl pkg-config  dbus ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      }
-    );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          naersk-lib = pkgs.callPackage naersk { };
+        in
+        {
+          default = naersk-lib.buildPackage {
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              openssl
+              pkg-config
+              dbus
+            ];
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
+            ];
+            nativeBuildInputs = with pkgs; [
+              openssl
+              pkg-config
+              dbus
+            ];
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: (import nixpkgs { inherit system; }).nixfmt-rfc-style);
+    };
 }
